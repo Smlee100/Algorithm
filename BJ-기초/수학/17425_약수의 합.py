c466759460c5bda9883c350b78727cee395f1d40
Smlee100 <smlee3243@gmail.com>
2021-08-10 10:20:22 +0900

    수학_약수 커밋

--- BJ-기초/수학/17425_약수의 합.py
+++ BJ-기초/수학/17425_약수의 합.py
@@ -1,4 +1,22 @@
-# 테스트케이스 주어진 경우
+# 테스트케이스 주어진 경우 ->시간복잡도 문제 생김
+# 약수의 개수는 약수가 아닌 수의 개수보다 매우 작다
+# part1: f[i]를 구한다 / part2: 입출력 구현
+
+MAX = 1000000
+f = [1]*(MAX+1)
+g = [0]*(MAX+1)
+for i in range(2,MAX+1):
+    j = 1
+    while i*j <= MAX:
+        f[i*j] = f[i*j] + i
+        j = j + 1
+for j in range(1,MAX+1):
+    g[i] = g[i-1] + f[i]
 
 T = int(input())
+ans = []
+for i in range(1,T+1):
+    N = int(input())
+    ans.append(g[N])
+print('\n'.join(map(str,ans))+'\n')
 
